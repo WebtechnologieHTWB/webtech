0b7ad92c5d76bf113dc7f8187005c2f802a08cfc
Dustin <s0569494@outlook.com>
2020-03-26 22:34:16 +0100

    fertige Abgabe?

---
+++ ueb6/auswahl.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$door_id = $_POST["door_id"];
+
+$goat_door = random_int(0,2);
+
+
+while ($goat_door == $door_id){
+    $goat_door = random_int(0,2);
+}
+
+echo $goat_door;
+?>
--- ueb6/main.js
+++ ueb6/main.js
@@ -9,20 +9,23 @@ function answer(id, x) {
     var min = 0, max = 1;
     var zufahl = Math.floor(Math.random() * (max - min + 1)) + min;
 
+    var playerScore = 0;
+    var computerScore = 0;
+
     switch (v) {
         case 0:
             if (id == 0) {
 
-                if (zufahl == 0) { document.getElementById('2').src = 'pic/ziege.jpg'; }
-                else { document.getElementById('1').src = 'pic/ziege.jpg'; }
+                if (zufahl == 0) { document.getElementById('2').src = './pic/ziege.jpg'; }
+                else { document.getElementById('1').src = './pic/ziege.jpg'; }
 
             } else if (id == 1) {
-                if (zufahl == 0) { document.getElementById('0').src = 'pic/ziege.jpg'; }
-                else { document.getElementById('2').src = 'pic/ziege.jpg'; }
+                if (zufahl == 0) { document.getElementById('0').src = './pic/ziege.jpg'; }
+                else { document.getElementById('2').src = './pic/ziege.jpg'; }
 
             } else if (id == 2) {
-                if (zufahl == 0) { document.getElementById('1').src = 'pic/ziege.jpg'; }
-                else { document.getElementById('0').src = 'pic/ziege.jpg'; }
+                if (zufahl == 0) { document.getElementById('1').src = './pic/ziege.jpg'; }
+                else { document.getElementById('0').src = './pic/ziege.jpg'; }
 
             }
 
@@ -30,14 +33,14 @@ function answer(id, x) {
             break;
         case 1:
             if (id == auto) {
-                if (id == 0) { document.getElementById('0').src = 'pic/auto.jpg'; }
-                else if (id == 1) { document.getElementById('1').src = 'pic/auto.jpg'; }
-                else if (id == 2) { document.getElementById('2').src = 'pic/auto.jpg'; }
+                if (id == 0) { document.getElementById('0').src = './pic/auto.jpg'; }
+                else if (id == 1) { document.getElementById('1').src = './pic/auto.jpg'; }
+                else if (id == 2) { document.getElementById('2').src = './pic/auto.jpg'; }
 
             } else if (id != auto) {
-                if (id == 0) { document.getElementById('0').src = 'pic/ziege.jpg'; }
-                else if (id == 1) { document.getElementById('1').src = 'pic/ziege.jpg'; }
-                else if (id == 2) { document.getElementById('2').src = 'pic/ziege.jpg'; }
+                if (id == 0) { document.getElementById('0').src = './pic/ziege.jpg'; }
+                else if (id == 1) { document.getElementById('1').src = './pic/ziege.jpg'; }
+                else if (id == 2) { document.getElementById('2').src = './pic/ziege.jpg'; }
 
             }
             v++;
@@ -45,14 +48,22 @@ function answer(id, x) {
         default:
             break;
     }
+
+    if (document.getElementById('0').src == './pic/auto.jpg' || document.getElementById('1').src == './pic/auto.jpg' || document.getElementById('2').src == './pic/auto.jpg') {
+        playerScore + 1;
+    }
+    else {
+        computerScore + 1;
+    }
+    document.getElementById('score').innerText = ' Player: ' + playerScore + ' and Computer: ' + computerScore;
 }
 
 function loadDoc() {
     v = 0;
 
-    document.getElementById('0').src = 'pic/door.jpg';
-    document.getElementById('1').src = 'pic/door.jpg';
-    document.getElementById('2').src = 'pic/door.jpg';
+    document.getElementById('0').src = './pic/door.jpg';
+    document.getElementById('1').src = './pic/door.jpg';
+    document.getElementById('2').src = './pic/door.jpg';
 
     answer(id, x);
 }
